ae969daaf624e2a54feed9f5e264f8a5ee09888a
Lewis <zachary.lewis@sony.com>
2020-09-02 12:13:53 -0700

    Updated icons and styling.

--- src/components/console.tsx
+++ src/components/console.tsx
@@ -38,6 +38,32 @@ export class Console extends Component<ConsoleProps, ConsoleState> {
       ? 'console expanded'
       : 'console collapsed';
 
+    const expansionIcon = this.state.expanded ? (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ) : (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
+          clipRule="evenodd"
+        />
+      </svg>
+    );
+
     return (
       <section className="console">
         <span className="prompt">{this.props.prompt || '>'}</span>
@@ -46,7 +72,7 @@ export class Console extends Component<ConsoleProps, ConsoleState> {
           className="expansion-toggle"
           onClick={() => this.handleExpansion()}
         >
-          {this.state.expanded ? '-' : '+'}
+          {expansionIcon}
         </button>
       </section>
     );
